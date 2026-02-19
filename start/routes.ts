1dad5bec848780d727a90948080df82c4fc02f20
Joel Nkiere <nkierejoe@gmail.com>
2026-02-19 20:21:33 +0100

    creation de la page le liste des discussion

--- start/routes.ts
+++ start/routes.ts
@@ -12,6 +12,8 @@ import router from '@adonisjs/core/services/router'
 const UsersController = () => import('#controllers/users_controller')
 const MessagesController = () => import('#controllers/messages_controller')
 const StatusController = () => import('#controllers/status_controller')
+const GroupsController = () => import('#controllers/groups_controller')
+
 
 // Inscription et Connexion
 router.post('/register', [UsersController, 'register'])
@@ -26,6 +28,7 @@ router.get('/users/:id', [UsersController, 'show'])      // Voir le profil d'un 
 
 
 // messages
+router.on('/messages').render('pages/messages') // page de messagerie (peut être utilisée pour afficher les conversations récentes, etc.)
 router.post('/messages/send/:receiverId', [MessagesController, 'send']) // Envoyer un message
 router.get('/messages/chat/:receiverId', [MessagesController, 'index']) // Récupérer la conversation avec quelqu'un
 router.put('/messages/read/:senderId', [MessagesController, 'markAsRead']) // Marquer les messages comme "lus"
@@ -38,3 +41,15 @@ router.get('/status/feed', [StatusController, 'index']) // Voir les statuts de s
 router.post('/status/like/:statusId', [StatusController, 'toggleLike']) // Liker / Unliker un statut
 router.get('/status/views/:statusId', [StatusController, 'getViews']) // Voir qui a regardé mon statut
 router.delete('/status/delete/:statusId', [StatusController, 'destroy'])// Supprimer son propre statut
+
+
+// groupes
+router.post('/groups/create', [GroupsController, 'store']) // Créer le groupe
+router.get('/groups/info/:groupId', [GroupsController, 'show']) // Infos du groupe (Membres, nom, image)
+router.post('/groups/send/:groupId', [GroupsController, 'sendMessage']) // Envoyer un message dans le groupe
+router.get('/groups/messages/:groupId', [GroupsController, 'getMessages']) // Récupérer les messages du groupe
+router.post('/groups/members/add/:groupId/:userId', [GroupsController, 'addMember']) // ajouter un membre au groupe
+router.delete('/groups/members/remove/:groupId/:userId', [GroupsController, 'removeMember']) // Retirer un membre du groupe
+router.post('/groups/leave/:groupId', [GroupsController, 'leave']) // Quitter un groupe
+router.delete('/groups/delete/:groupId', [GroupsController, 'destroy']) // Supprimer un groupe
+
